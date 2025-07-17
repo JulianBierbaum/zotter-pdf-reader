e1559c10e9bedfc98ce673888797a4182475232f
Julian Bierbaum <bierbaumjulian@gmail.com>
2025-07-17 15:29:11 +0200

    updated script and compose

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ cd model
 docker build -t julianbierbaum/repo:ollama-gemma .
 docker push julianbierbaum/repo:ollama-gemma
 
+cd ..
+
 cd website
 docker build -t julianbierbaum/repo:next-app .
 docker push julianbierbaum/repo:next-app
